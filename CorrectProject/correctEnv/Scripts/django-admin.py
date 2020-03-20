6c5d3be53fcb1ee8b84668b6e6a0a6e34563c95e
arsalybek <askarbekovnav@gmail.com>
2020-03-20 12:53:37 +0600

    correct Project is added

---
+++ CorrectProject/correctEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Админ\Documents\WD(Django)\WebBackEnd\CorrectProject\correctEnv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
